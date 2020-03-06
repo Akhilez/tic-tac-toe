d4bf7f2d7606e9775f5afeb08e7c64005580c0b6
Akhilez <akhild18@yahoo.com>
2020-03-06 17:43:03 -0500

    Added some player swapping but didn't improve results

--- main.py
+++ main.py
@@ -12,17 +12,29 @@ data_manager.get()
 
 dense_player = PolicyGradPlayer('PolicyGrad', Frame.X)
 player2 = RandomPlayer('Random', Frame.O)
-# player2 = PerfectPlayer('Static', Frame.O)
+player3 = PerfectPlayer('Static', Frame.O)
 
 game = Game(dense_player, player2)
 
-for i in range(10):
-    game.start(10)
-    data_manager.enqueue(game.matches)
-    dense_player.train(50, data_manager)
-    dense_player.save_params()
-    game.matches.clear()
-    game.swap_players()
+for i in range(5):
+    for j in range(4):
+        if j == 0:
+            game.player_1 = dense_player
+            game.player_2 = player2
+        elif j == 1:
+            game.player_1 = dense_player
+            game.player_2 = player3
+        elif j == 2:
+            game.player_1 = player2
+            game.player_2 = dense_player
+        elif j == 3:
+            game.player_1 = player3
+            game.player_2 = dense_player
+        game.start(10)
+        data_manager.enqueue(game.matches)
+        dense_player.train(50, data_manager)
+        dense_player.save_params()
+        game.matches.clear()
+        # game.swap_players()
 
 data_manager.write()
-
